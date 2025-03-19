ba5c3d0d14c155fa987c56536b7f3efb06b2d10a
Kamil <kamil.arslanoffx@gmail.com>
2025-03-19 18:23:26 +0500

    -

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -14,63 +14,64 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createSQLQuery("""
-                CREATE TABLE IF NOT EXISTS users (
-                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR(50) NOT NULL,
-                    lastName VARCHAR(50) NOT NULL,
-                    age TINYINT NOT NULL
-                )
-            """).executeUpdate();
-            transaction.commit();
-        }
+//        try (Session session = Util.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            session.createSQLQuery("""
+//                CREATE TABLE IF NOT EXISTS users (
+//                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+//                    name VARCHAR(50) NOT NULL,
+//                    lastName VARCHAR(50) NOT NULL,
+//                    age TINYINT NOT NULL
+//                )
+//            """).executeUpdate();
+//            transaction.commit();
+//        }
     }
 
     @Override
     public void dropUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-            transaction.commit();
-        }
+//        try (Session session = Util.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+//            transaction.commit();
+//        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try(Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.persist(new User(name, lastName, age));
-            transaction.commit();
-        }
+//        try(Session session = Util.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            session.persist(new User(name, lastName, age));
+//            transaction.commit();
+//        }
     }
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            User user = session.get(User.class, id);
-            if (user != null) {
-                session.remove(user);
-            }
-            transaction.commit();
-        }
+//        try (Session session = Util.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            User user = session.get(User.class, id);
+//            if (user != null) {
+//                session.remove(user);
+//            }
+//            transaction.commit();
+//        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            return session.createQuery("FROM User", User.class).list();
-        }
+//        try (Session session = Util.getSessionFactory().openSession()) {
+//            return session.createQuery("FROM User", User.class).list();
+//        }
+        return null;
     }
 
     @Override
     public void cleanUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createQuery("DELETE FROM User").executeUpdate();
-            transaction.commit();
-        }
+//        try (Session session = Util.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            session.createQuery("DELETE FROM User").executeUpdate();
+//            transaction.commit();
+//        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,50 +1,10 @@
 package jm.task.core.jdbc.util;
 
-import jm.task.core.jdbc.model.User;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.service.ServiceRegistry;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Util {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-            Configuration configuration = new Configuration();
-
-            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-            configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/main");
-            configuration.setProperty("hibernate.connection.username", "root");
-            configuration.setProperty("hibernate.connection.password", "1234");
-
-            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-
-            configuration.setProperty("hibernate.hbm2ddl.auto", "update"); // update / create / validate / none
-            configuration.setProperty("hibernate.show_sql", "true");
-
-            configuration.addAnnotatedClass(User.class);
-
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-
-            return configuration.buildSessionFactory(serviceRegistry);
-        } catch (Exception ex) {
-            throw new ExceptionInInitializerError("Ошибка инициализации Hibernate: " + ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-    public static void shutdown() {
-        getSessionFactory().close();
-    }
-
     public static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/main";
         String user = "root";
